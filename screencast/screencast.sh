ec6fb285edcba7371ec722a7e8647c045c220c09
Sandeep Nanajkar <sandeep.n@imaginea.com>
2014-07-18 17:13:21 +0530

    partial fixes for blurring

--- screencast/screencast.sh
+++ screencast/screencast.sh
@@ -5,8 +5,6 @@
 ##################################################
 
 #!/bin/sh
-echo "Enter the output file name: "; read name
-
 scriptPath() {
     pushd . > /dev/null
     SCRIPT_PATH="${BASH_SOURCE[0]}";
@@ -21,7 +19,10 @@ scriptPath() {
 }
 
 scrcastHome="$(scriptPath)/Videos"
-echo "${scrcastHome}"
+#echo "${scrcastHome}"
+name="sc-`date +'%m%d%Y%-H%M%S'`"
+#echo "Enter the output file name: "; read name
+echo "output file ${scrcastHome}/${name}"
 mkdir -p "${scrcastHome}/tmp"
 interimName="${scrcastHome}/tmp/${name}-interim-xxyy.mp4"
 mergedClean="${scrcastHome}/tmp/${name}-mergedclean-xxyy.mp4"
@@ -35,13 +36,17 @@ watermark="pramati.png"
 
 fullscreen=$(xwininfo -root | grep 'geometry' | awk '{print $2;}')
 #get the selfie video
-avconv -f video4linux2 -s 5x5 -i /dev/video0 ${selfieVideo} > /dev/null 2>&1 &
+#avconv -f video4linux2 -s 5x5 -i /dev/video0 ${selfieVideo} > /dev/null 2>&1 &
+avconv -f video4linux2 -s 10x10 -r 60 -i /dev/video0 -vf "unsharp=luma_msize_x=7:luma_msize_y=7:luma_amount=2.5" -codec:v libx264 -vprofile main ${selfieVideo} > /dev/null 2>&1 &
 selfie_pid=$_
 echo "started the selfie(${selfie_pid}). . ."
 
 #start screencast
 echo "start the screecast grab. . ." 
-avconv -f alsa -i pulse -f x11grab -r 30 -s $fullscreen -i :0.0 -vcodec libx264 -preset ultrafast -threads 4 -y ${interimName}
+#lossless_ultrafast="-crf 0 no-8x8dct aq-mode 0 b-adapt 0 bframes 0 no-cabac no-debloc no-mbtre me d no-mixed-refs partitions none ref 1 scenecut 0 subme  trellis no-weightb weightp 0"
+#avconv -f alsa -i pulse -f x11grab -r 24 -s $fullscreen -vf "yadif" -b:v 6144k -i :0.0 -vcodec libx264 -preset slow -threads 5 -y ${interimName}
+#ffmpeg -f x11grab -s SZ -r 30 -i :0.0 -qscale 0 -vcodec huffyuv grab.avi
+avconv -f alsa -i pulse -f x11grab -r 5 -s $fullscreen -same_quant -i :0.0 -vcodec libx264 -preset ultrafast -threads 4 -y ${interimName}
 echo "screecast grab ended."
 kill ${selfie_pid}
 echo "selfie killed"
@@ -55,15 +60,15 @@ echo "audio video splited. . ."
 sox ${onlyAudio} ${onlyAudioCleaned} noisered noise.prof 0.21
 
 #merge back the cleaned audio
-avconv -i ${onlyAudioCleaned} -i ${onlyVideo} -same_quant ${mergedClean}
+avconv -i ${onlyAudioCleaned} -i ${onlyVideo} -same_quant -y ${mergedClean}
 echo "noise cancellation applied and merged. . ." 
 
 #merge the selfie
-avconv -i ${mergedClean} -vf "movie=${selfieVideo}[inner];[in][inner] overlay=main_w-overlay_w-10:main_h-overlay_h-10 [out]" ${mergedSelfieVideo}
+avconv -same_quant -i ${mergedClean} -vf "movie=${selfieVideo}[inner];[in][inner] overlay=main_w-overlay_w-10:main_h-overlay_h-10 [out]" -y ${mergedSelfieVideo}
 echo "selfie merged"
 
 #add watermark
-avconv -i ${mergedSelfieVideo} -vf "movie=${watermark} [watermark];[in][watermark] overlay=main_w-overlay_w-15:main_h-overlay_h-600 [out]" ${finalName}
+avconv -same_quant -i ${mergedSelfieVideo} -vf "movie=${watermark} [watermark];[in][watermark] overlay=main_w-overlay_w-15:main_h-overlay_h-600 [out]" -y ${finalName}
 echo "watermark merged"
 
 #cleanup
